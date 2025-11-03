384761e9fe21ea67977e57abc7edb6a5d8cebe58
Git-Hub-Matthew <105342121@student.swin.edu.au>
2025-11-03 22:46:01 +1100

    Add files via upload

---
+++ frontend/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello, welcome to my React app!</h1>
+            <p>This is a custom message replacing the default content.</p>
+        </div>
+    );
+}
+export default App;
